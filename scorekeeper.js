0855db3fe80362d92824623556eafa88db33498f
Morgan N <mzartdesigns@gmail.com>
2017-09-17 17:10:32 -0400

    Added stylesheet and added more JS
    Added logic where if one of the players reaches the winning score, the
    game is over and they’ll have to reset the scores.

--- scorekeeper.js
+++ scorekeeper.js
@@ -5,14 +5,38 @@ var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
 var p2Score = 0;
 
+var gameOver = false;
+var winningScore = 5;
+
+var resetScore = document.querySelector("#reset");
+
+
 
 p1Button.addEventListener("click", function(){
+  if(!gameOver){
     p1Score++;
-    console.log(p1Score);
+    if(p1Score === winningScore) {
+      gameOver = true;
+    }
     p1Display.textContent = p1Score;
+  }
 });
 
 p2button.addEventListener("click", function() {
-  p2Score++;
+  if(!gameOver){
+    p2Score++;
+    if(p2Score === winningScore){
+      gameOver = true;
+    }
+    p2Display.textContent = p2Score;
+  }
+
+});
+
+resetScore.addEventListener("click", function() {
+  p1Score = 0;
+  p1Display.textContent = p1Score;
+
+  p2Score = 0;
   p2Display.textContent = p2Score;
 });
